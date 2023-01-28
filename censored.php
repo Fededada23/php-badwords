cc2b88faef8fd41701bbc8c8e9a5e4b795f63e76
Fededada23 <dadamofederico@gmail.com>
2023-01-28 15:33:16 +0100

    first commit

---
+++ censored.php
@@ -0,0 +1,37 @@
+<?php
+$paragraph = $_GET['paragraph'];
+$censored = $_GET['censored'];
+$censored_string = str_replace($censored,'***', $paragraph);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel ="stylesheet" href="./css/style.css">
+    <title>php</title>
+</head>
+    <body>
+        <div class="center">
+            <div>
+                <h1>
+                    La tua frase è:
+                </h1>
+                <h1>
+                    <?php echo $paragraph."<br> La tua frase è lunga: ".strlen($paragraph)." caratteri.";?>
+                </h1>
+            </div>
+            <div>
+                <h1>
+                    La tua frase censurata è:
+                </h1>
+                <h1>
+                    <?php echo $censored_string."<br> La tua frase censurata è lunga: ".strlen($censored_string)." caratteri.";?>
+                </h1>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel ="stylesheet" href="./css/style.css">
+        <title>Primo eserecizio php</title>
+</head>
+    <body>
+        <div class="center">
+            <h3>Inserisci una frase ed una parola da censurare:</h3>
+            <form action="./censored.php" method="GET">
+                <input type="text" name="paragraph" id="paragraph"
+                placeholder="paragraph">
+                <input type="text" name="censored" id="censored"
+                placeholder="censored">
+                <button type="submit">Censura</button>
+            </form>
+        </div>
+    </body>
+</html>
